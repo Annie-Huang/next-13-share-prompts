ae2bf38d69520556b52e1dec5e4df6cc55d28a1b
Annie Huang <annie.hn@gmail.com>
2023-10-02 17:33:22 +1100

    Create the content for app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
 const Home = () => {
-  return <div>Home</div>;
+  // .head_text is defined in the global.css. all the "_" class is from the global.css
+  return (
+    <section class='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover & Share <br class='max-md:hidden' />
+        <span class='orange_gradient text-center'>AI-Powered Prompts</span>
+      </h1>
+      <p class='desc text-center'>
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
