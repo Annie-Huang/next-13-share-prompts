69033006c4f94fa07c5f5535f0e7979b909490f0
Annie Huang <annie.hn@gmail.com>
2023-10-02 17:24:27 +1100

    Create app/page.jsx and app/layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import '@/styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div class='main'>
+          <div class='gradient'></div>
+        </div>
+        <main class='app'>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
