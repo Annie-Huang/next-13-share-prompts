dff40947e4ba1db845463b873cac7ad7c8d56b3d
Annie Huang <annie.hn@gmail.com>
2023-10-03 16:54:42 +1100

    Create providers local variable and set it from next-auth when the page is launched

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,10 +2,23 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { signOut } from 'next-auth/react';
+import { getProviders, signOut } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+
+    setProviders();
+  }, []);
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
