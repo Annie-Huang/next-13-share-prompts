971df7c1ca78ea20b851c52a2073ee1973f45b81
Annie Huang <annie.hn@gmail.com>
2023-10-04 09:13:39 +1100

    Create the top right side for header when it's in mobile view.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { getProviders, signIn, signOut } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 
 const Nav = () => {
-  const isUserLoggedIn = false;
+  const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
 
   useEffect(() => {
@@ -52,6 +52,36 @@ const Nav = () => {
                 alt='profile'
               />
             </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className='sm:hidden flex relative'>
+        {isUserLoggedIn ? (
+          <div className='flex'>
+            <Image
+              src='/assets/images/logo.svg'
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='profile'
+              onClick={() => {}}
+            />
           </div>
         ) : (
           <>
