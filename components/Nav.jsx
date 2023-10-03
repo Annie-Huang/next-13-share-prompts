c203554c2c2a3d916f357d52f7941fb8be42f796
Annie Huang <annie.hn@gmail.com>
2023-10-03 16:48:18 +1100

    Add the Sign Out button and Profile image as well (use logo for now)

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
+import { signOut } from 'next-auth/react';
 
 const Nav = () => {
   const isUserLoggedIn = true;
@@ -21,6 +24,20 @@ const Nav = () => {
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Post
+            </Link>
+
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+
+            <Link href='/profile'>
+              <Image
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+              />
             </Link>
           </div>
         ) : (
