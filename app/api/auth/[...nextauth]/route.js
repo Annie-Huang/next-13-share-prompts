5197a50dd4679a5047b3a99812cb7c3bb81f3a54
Annie Huang <annie.hn@gmail.com>
2023-10-04 12:06:57 +1100

    Add the GOOGLE_ID and GOOGLE_CLIENT_SECRET into .env and add into into app/api/auth/[...nextauth]/route.js file

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,18 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+// console.log({
+//   childId: process.env.GOOGLE_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+// });
+
 const handler = NextAuth({
-  providers: [GoogleProvider({ childId: '', clientSecret: '' })],
+  providers: [
+    GoogleProvider({
+      childId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
   async session({ session }) {},
   async signIn({ profile }) {},
 });
